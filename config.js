08fb8d9552d635c8fb4b46d75243a98e754b1fdf
Evan Liu/刘彬 <103646150+evanlliu@users.noreply.github.com>
2026-09-23 17:09:41 -0600

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ window.APP_CONFIG = {
     branch: "data",
     path: "data.json",
 
-    tokenCiphertext: "Sr+oZO4zPl8Q6pyueOToqkZRWaY6x8I1a9+KMzWTXdfH9qqayJPCRzNRRyQCLSdYF4yr0/Q6myGBzsl3s4u+GpwC/eYHevVnn7+ZscEQKUc656FrUpV8TQ4rw6FfIRfk+qksK63z/Veg/rtsQp0DaxQFbk5kW5PU3A==",
+    tokenCiphertext: "3zhoaM6Ji6llx7Htk/2b/9ts6JR8Gj5GIBxUQaXxYEmlG7Ol36V/hQ4AjAIbGXRT+9cCvk/7TFP9pMAr6krC139j2n/6HHWU9ZsETt2r9xoPmeF9NnClVFKmSK+t4DF8klRhhlVuIXl3HBXs9xIR6FU83eBXZ1KcGw==",
 
     keyMaterial: "midautumn-2026-x7K9p2Qa"
   }
